b0eae80205cc5f3e35eb26459887f0ccb4803905
Pedreduardo <pedroeduardo.contato@gmail.com>
2016-05-26 16:13:44 -0300

    Classe (modelo json) de filme e searchable na toolbar

--- app/src/main/java/dutest/omdb/activity/MovieProfile.java
+++ app/src/main/java/dutest/omdb/activity/MovieProfile.java
@@ -1,16 +1,23 @@
 package dutest.omdb.activity;
 
+import android.app.SearchManager;
 import android.content.Context;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import dutest.omdb.R;
 import dutest.omdb.util.image.DownloadImageTask;
@@ -44,9 +51,7 @@ public class MovieProfile extends AppCompatActivity{
         this.posterImage = (ImageView) findViewById(R.id.posterImage);
 
         setSupportActionBar(this.toolbar);
-        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
-        upArrow.setColorFilter(ContextCompat.getColor(this.context, R.color.white), PorterDuff.Mode.SRC_ATOP);
-        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         this.collapsing.setTitle("Harry Potter and the Deathly Hallows");
         this.collapsing.setCollapsedTitleTextColor(ContextCompat.getColor(this.context, R.color.white));
@@ -56,6 +61,45 @@ public class MovieProfile extends AppCompatActivity{
         new DownloadImageTask(this.posterImage).execute("http://ia.media-imdb.com/images/M/MV5BMTY2MTk3MDQ1N15BMl5BanBnXkFtZTcwMzI4NzA2NQ@@._V1_SX300.jpg");
 
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.movie_profile_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager) MovieProfile.this.getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView searchView = null;
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(MovieProfile.this.getComponentName()));
+        }
+
+        EditText searchText = ((EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
+        searchText.setHintTextColor(ContextCompat.getColor(this.context, R.color.white));
+        searchText.setTextColor(ContextCompat.getColor(this.context, R.color.white));
+
+        SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+            public boolean onQueryTextChange(String newText) {return true; }
+
+            public boolean onQueryTextSubmit(String query) {
+                String amazonSearch = "http://www.amazon.com/gp/mas/dl/android?s=";
+                query = query.replace(' ', '+');
+                amazonSearch += query + "+dvd+bluray";
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(amazonSearch));
+                startActivity(browserIntent);
+                return true;
+            }
+        };
+        searchView.setOnQueryTextListener(queryTextListener);
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 
 
---
+++ app/src/main/java/dutest/omdb/model/json/Movie.java
@@ -0,0 +1,88 @@
+package dutest.omdb.model.json;
+
+/**
+ * Created by Pedreduardo on 26/05/2016.
+ */
+public class Movie {
+
+    String response;
+    String Title;
+    String Plot;
+    String Poster;
+    String imdbRating;
+    String imdbVotes;
+    String Metascore;
+    String Awards;
+    String Year;
+    String Rated;
+    String Genre;
+
+
+    public String getResponse() {
+        return response;
+    }
+    public void setResponse(String response) {
+        this.response = response;
+    }
+    public String getTitle() {
+        return Title;
+    }
+    public void setTitle(String title) {
+        Title = title;
+    }
+    public String getPlot() {
+        return Plot;
+    }
+    public void setPlot(String plot) {
+        Plot = plot;
+    }
+    public String getPoster() {
+        return Poster;
+    }
+    public void setPoster(String poster) {
+        Poster = poster;
+    }
+    public String getImdbRating() {
+        return imdbRating;
+    }
+    public void setImdbRating(String imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+    public String getImdbVotes() {
+        return imdbVotes;
+    }
+    public void setImdbVotes(String imdbVotes) {
+        this.imdbVotes = imdbVotes;
+    }
+    public String getMetascore() {
+        return Metascore;
+    }
+    public void setMetascore(String metascore) {
+        Metascore = metascore;
+    }
+    public String getAwards() {
+        return Awards;
+    }
+    public void setAwards(String awards) {
+        Awards = awards;
+    }
+    public String getYear() {
+        return Year;
+    }
+    public void setYear(String year) {
+        Year = year;
+    }
+    public String getRated() {
+        return Rated;
+    }
+    public void setRated(String rated) {
+        Rated = rated;
+    }
+    public String getGenre() {
+        return Genre;
+    }
+    public void setGenre(String genre) {
+        Genre = genre;
+    }
+
+}
